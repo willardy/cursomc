34f882066300ba98779a7c370348cfc57f8d51ad
Willardy Tyrone <willardy@gmail.com>
2020-10-25 11:01:42 -0300

    Criado classe Produto e associação com categoria de muitos para muitos

---
+++ src/main/java/com/willardy/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.willardy.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.willardy.cursomc.domain.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
