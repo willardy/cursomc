1a6c2c70a58b2961157372b5b7bdc128af193058
Willardy Tyrone <willardy@gmail.com>
2020-10-25 11:34:43 -0300

    Ajustes no endpoint  /categoria/{id}

--- src/main/java/com/willardy/cursomc/services/CategoriaService.java
+++ src/main/java/com/willardy/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.willardy.cursomc.domain.Categoria;
 import com.willardy.cursomc.repositories.CategoriaRepository;
+import com.willardy.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,8 +18,7 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		
-		return obj.orElse(null);
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id:" + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
