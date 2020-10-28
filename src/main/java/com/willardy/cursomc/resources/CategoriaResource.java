80733d36d6f0d6ea70d502b70760ec5c2cc5859c
Willardy Tyrone <willardy@gmail.com>
2020-10-27 22:20:42 -0300

    Incluido metodo POST para Categoria

--- src/main/java/com/willardy/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/willardy/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,17 @@
 package com.willardy.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.willardy.cursomc.domain.Categoria;
 import com.willardy.cursomc.services.CategoriaService;
@@ -25,5 +28,13 @@ public class CategoriaResource {
 		Categoria obj = service.buscar(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria){
+		categoria = service.insert(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/willardy/cursomc/services/CategoriaService.java
+++ src/main/java/com/willardy/cursomc/services/CategoriaService.java
@@ -21,4 +21,8 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id:" + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
+	public Categoria insert(Categoria categoria) {
+		return repo.save(categoria);
+	}
+	
 }
