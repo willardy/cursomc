cac7b3b3bc15037f959d150e6d118fdf9c1adf87
Willardy Tyrone <willardy@gmail.com>
2020-10-30 11:49:20 -0300

    Paginação com commits opcionais na requisição

--- src/main/java/com/willardy/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/willardy/cursomc/resources/CategoriaResource.java
@@ -2,15 +2,16 @@ package com.willardy.cursomc.resources;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -59,6 +60,19 @@ public class CategoriaResource {
 	public ResponseEntity<List<CategoriaDTO>> findAll(){
 		List<Categoria> listCategoria = service.findAll();
 		List<CategoriaDTO> listCategoriaDTO = listCategoria.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listCategoriaDTO);
+	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(name = "page", defaultValue = "0") Integer page, 
+			@RequestParam(name = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(name = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(name = "direction", defaultValue = "ASC") String direction
+			){
+		Page<Categoria> listCategoria = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listCategoriaDTO = listCategoria.map(categoria -> new CategoriaDTO(categoria));
 		
 		return ResponseEntity.ok().body(listCategoriaDTO);
 	}
