08650989ec603392626a2ad6b8ccb2cc9de9ed1a
Willardy Tyrone <willardy@gmail.com>
2020-11-01 19:22:41 -0300

    Correção na classe CategoriaService

--- src/main/java/com/willardy/cursomc/services/CategoriaService.java
+++ src/main/java/com/willardy/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.willardy.cursomc.domain.Categoria;
+import com.willardy.cursomc.domain.Categoria;
 import com.willardy.cursomc.dto.CategoriaDTO;
 import com.willardy.cursomc.repositories.CategoriaRepository;
 import com.willardy.cursomc.services.exceptions.DataIntegrityException;
@@ -35,8 +36,9 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -59,6 +61,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 
 }
