f4b5913bfda743c94594adec44cf3c9714534bf6
Willardy Tyrone <willardy@gmail.com>
2020-11-01 20:19:45 -0300

    Ajustando o método de DELETE do Cliente

--- src/main/java/com/willardy/cursomc/resources/ClienteResource.java
+++ src/main/java/com/willardy/cursomc/resources/ClienteResource.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.willardy.cursomc.domain.Categoria;
 import com.willardy.cursomc.domain.Cliente;
 import com.willardy.cursomc.dto.ClienteDTO;
 import com.willardy.cursomc.dto.ClienteNewDTO;
